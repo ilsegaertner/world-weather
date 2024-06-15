31231dc746ade73af5d54807293ba11b140791de
Ilse Gaertner <joachimpruegl@gmail.com>
2024-06-15 15:47:51 +0200

    Delete comments, add api keys

--- extension/main.js
+++ extension/main.js
@@ -2,7 +2,7 @@ console.log("The extension is up and running");
 
 var images = document.getElementsByTagName("img");
 
-for (let elt of images) {
-  elt.src = `${chrome.runtime.getURL("logo.png")}`;
-  elt.alt = "an alt text";
-}
+// for (let elt of images) {
+//   elt.src = `${chrome.runtime.getURL("logo.png")}`;
+//   elt.alt = "an alt text";
+// }
