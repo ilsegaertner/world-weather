f891b1843f2e8855a72ba901f78c21009fa454bc
Ilse Gaertner <joachimpruegl@gmail.com>
2024-06-24 08:13:18 +0200

    Refactor weather component with new radix themes, sonner, framer motion

--- src/Components/weather/weather.jsx
+++ src/Components/weather/weather.jsx
@@ -4,6 +4,11 @@ import RainSvg from "../../assets/amcharts_weather_icons_1.0.0/static/rainy-7.sv
 import CloudsSvg from "../../assets/amcharts_weather_icons_1.0.0/static/cloudy.svg";
 import DrizzleSvg from "../../assets/amcharts_weather_icons_1.0.0/static/rainy-6.svg";
 import ClearSvg from "../../assets/amcharts_weather_icons_1.0.0/static/day.svg";
+import { Flex, Text, Button, Box, Spinner } from "@radix-ui/themes";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import HeadingSecond from "../heading-second/heading-second";
+import { motion } from "framer-motion";
+import { Toaster } from "sonner";
 
 const Weather = ({
   weatherData,
@@ -22,7 +27,7 @@ const Weather = ({
       const timeoutId = setTimeout(() => {
         setQuery("");
         setError(null);
-      }, 1500);
+      }, 1300);
       return () => clearTimeout(timeoutId);
     }
   }, [error, setError, setQuery]);
@@ -30,28 +35,22 @@ const Weather = ({
   if (error) {
     return (
       <>
-        <div className="absolute bg-gray-900 p-5 text-red-700">
-          Error: {error}
-        </div>
+        <Flex
+          gap="3"
+          className="absolute bg-gray-900 p-5 text-red-700 rounded-md  "
+        >
+          <Text as="p" size="2" weight="medium">
+            {" "}
+            Error: {error}
+          </Text>
+          <Spinner size="2" />
+        </Flex>
       </>
     );
   }
 
   if (!weatherData) {
-    return (
-      <div
-        style={{
-          position: "absolute",
-          left: "20px",
-          color: "red",
-          backgroundColor: "black",
-          width: "650px",
-          zIndex: "100",
-        }}
-      >
-        Loading...
-      </div>
-    );
+    return <Spinner size="3"></Spinner>;
   }
 
   let backgroundImage;
@@ -79,60 +78,104 @@ const Weather = ({
 
   return (
     <>
-      <div className=" p-5 bg-gray-700 sm:p-2 shadow-xl border-gray-900 border-solid overflow-auto border-2 ">
-        <div className="weather-content grid grid-rows-2 grid-auto-rows-min gap-8">
-          <div className="heading-and-input grid-start-1 flex flex-col justify-between self-start gap-3">
-            <p className="text-3xl text-yellow-300 mb-3 flex">World Weather</p>
-            <form
-              className="flex gap-4 md:flex-row flex-col"
-              onSubmit={handleSubmit}
-            >
-              <input
-                className="bg-gray-100 border border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:border-blue-500 placeholder-gray-500 text-gray-800"
-                value={query}
-                onChange={inputQuery}
-                placeholder="Enter city"
-                onKeyDown={handleKeyDown}
-              />
-              <button
-                type="sumbit"
-                className="bg-blue-500 p-2 rounded-lg text-gray-100 hover:bg-blue-700"
-              >
-                Go
-              </button>
-            </form>
-          </div>
+      <motion.div
+        initial={{ x: -100 }}
+        exit={{ x: 100 }}
+        animate={{ x: 0 }}
+        layout
+        transition={{
+          duration: 0.03,
+          type: "spring",
+          stiffness: 549,
+          mass: 1,
+          damping: 32,
+        }}
+      >
+        <div className=" p-5 bg-gray-700 sm:p-2 shadow-xl border-gray-900 border-solid overflow-auto border-2 ">
+          <div className="weather-content grid grid-rows-2 grid-auto-rows-min gap-0">
+            <Flex direction="column">
+              <Box>
+                {/* <div className="heading-and-input grid-start-1 flex flex-col justify-between self-start gap-3"> */}
+                <Text
+                  as="p"
+                  size="5"
+                  weight="bold"
+                  className=" text-yellow-300 mb-3 flex"
+                >
+                  World Weather
+                </Text>
+              </Box>
+              <Box>
+                <form
+                  className="flex gap-4 md:flex-row flex-col"
+                  onSubmit={handleSubmit}
+                >
+                  <input
+                    className="bg-gray-100 border Input border-gray-300 rounded-md py-2 px-4 focus:outline-none focus:border-blue-500 placeholder-gray-500 text-gray-800"
+                    value={query}
+                    onChange={inputQuery}
+                    placeholder="Enter city"
+                    onKeyDown={handleKeyDown}
+                  />
+                  <Button
+                    type="sumbit"
+                    variant="classic"
+                    className="bg-blue-500 p-2 rounded-lg text-gray-100 hover:bg-blue-700"
+                  >
+                    <ArrowRightIcon />
+                  </Button>
+                </form>
 
-          <div>
-            {weatherData && (
-              <>
-                <div className="grid-start-2 self-center">
-                  <p className="city-and-country align-center flex text-lg text-gray-100 pl-2">
-                    {weatherData.name},{weatherData.sys.country}  {" "}
-                  </p>
+                {/* </div> */}
+              </Box>
+            </Flex>
+            <div>
+              {weatherData && (
+                <>
+                  <div className="grid-start-2 self-center">
+                    <Text
+                      as="div"
+                      size="2"
+                      weight="medium"
+                      className="city-and-country align-center flex text-gray-100 pl-2"
+                    >
+                      {weatherData.name},{weatherData.sys.country}  {" "}
+                    </Text>
 
-                  <div className="weather-image-and-temperature flex justify-between flex-row text-yellow-200 items-center overflow-none">
-                    <div className="p-1 justify-between align-top text-gray-100 flex flex-col items-center">
-                      <div
-                        className="relative w-16 h-16 bg-cover"
-                        style={{ backgroundImage }}
-                      ></div>
-                      <p>{weatherData.weather[0].main}</p>
+                    <div className="weather-image-and-temperature flex justify-between flex-row text-yellow-200 items-center overflow-none">
+                      <div className="p-1 justify-between align-top text-gray-100 flex flex-col items-center">
+                        <div
+                          className="relative w-16 h-16 bg-cover"
+                          style={{ backgroundImage }}
+                        ></div>
+                        <Text as="p" size="2" weight="light">
+                          {weatherData.weather[0].main}
+                        </Text>
+                      </div>
+                      <Text
+                        as="p"
+                        size="7"
+                        weight="medium"
+                        className=" text-white"
+                      >
+                        {temperatureCelsius.toFixed()}°C
+                      </Text>
+
+                      <div></div>
                     </div>
-                    <span className="text-4xl text-white">
-                      {temperatureCelsius.toFixed()}°C
-                    </span>
-
-                    <div></div>
                   </div>
-                </div>
-              </>
-            )}
+                </>
+              )}
 
-            {error && <p>Error: {error}</p>}
+              {error && (
+                <Text as="p" size="2" weight="light">
+                  Error: {error}
+                </Text>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
