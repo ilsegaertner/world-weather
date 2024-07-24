22fe9aec1e8b72aebeba21f5f75c62ba46ab4c1c
Ilse Gaertner <joachimpruegl@gmail.com>
2024-07-24 10:21:10 +0200

    Update legal-info.jsx

--- src/Components/legal-info/legal-info.jsx
+++ src/Components/legal-info/legal-info.jsx
@@ -1,135 +1,142 @@
 import React from "react";
+import { Text } from "@radix-ui/themes";
 import FooterComponent from "../footer-component/footer";
 
 const LegalInfo = () => {
   return (
     <>
-      <div className="p-48 bg-white md:px-24 lg:px-48 xl:px-64  xl:max-w-8xl ">
-        <h1 className=" text-xl">Legal Info</h1>
+      <div className="px-10 py-16 bg-white  ">
+        <Text as="div" size="3" align="left">
+          <h1 className=" text-xl">Legal Info</h1>
 
-        <p className="py-4">( Information in accordance with § 5 TMG )</p>
+          <p className="py-4">( Information in accordance with § 5 TMG )</p>
 
-        <p>
-          <strong>Imprint: </strong>
-        </p>
+          <p>
+            <strong>Imprint: </strong>
+          </p>
 
-        <address className="py-4">
-          World Weather App GmbH <br />
-          Romanovskystr. 34 <br />
-          10203 Berlin, Germany <br />
-        </address>
+          <address className="py-4">
+            World Weather App GmbH <br />
+            Romanovskystr. 34 <br />
+            10203 Berlin, Germany <br />
+          </address>
 
-        <p>
-          E-Mail:
-          <a href="mailto:info@worldweatherapp.com">info@worldweatherapp.com</a>
-          <br />
-          Internet address:
-          <a href="https://www.worldweatherapp.com">www.worldweatherapp.com</a>
-        </p>
+          <p>
+            E-Mail:
+            <a href="mailto:info@worldweatherapp.com">
+              info@worldweatherapp.com
+            </a>
+            <br />
+            Internet address:
+            <a href="https://www.worldweatherapp.com">
+              www.worldweatherapp.com
+            </a>
+          </p>
 
-        <p className="py-4">
-          <strong>Privacy Policy</strong>
-        </p>
-        <p>
-          This Privacy Policy describes how World-Weather-App ("we" or "us")
-          collects, uses, and discloses personal information obtained through
-          this website.
-        </p>
-        <p className="py-4">
-          Information Collection and Use: We collect personal information, such
-          as name and email address, through the contact form on this website.
-          We use this information to respond to inquiries and provide requested
-          services.
-        </p>
-        <p>
-          Information Sharing: We do not share personal information with third
-          parties unless required by law or with your consent.
-        </p>
+          <p className="py-4">
+            <strong>Privacy Policy</strong>
+          </p>
+          <p>
+            This Privacy Policy describes how World-Weather-App ("we" or "us")
+            collects, uses, and discloses personal information obtained through
+            this website.
+          </p>
+          <p className="py-4">
+            Information Collection and Use: We collect personal information,
+            such as name and email address, through the contact form on this
+            website. We use this information to respond to inquiries and provide
+            requested services.
+          </p>
+          <p>
+            Information Sharing: We do not share personal information with third
+            parties unless required by law or with your consent.
+          </p>
 
-        <p className="py-4">
-          Data Security: We take reasonable precautions to protect personal
-          information from loss, misuse, or unauthorized access.
-        </p>
-        <p>
-          Changes to this Privacy Policy: We may update this Privacy Policy from
-          time to time. The updated version will be posted on this page.
-        </p>
+          <p className="py-4">
+            Data Security: We take reasonable precautions to protect personal
+            information from loss, misuse, or unauthorized access.
+          </p>
+          <p>
+            Changes to this Privacy Policy: We may update this Privacy Policy
+            from time to time. The updated version will be posted on this page.
+          </p>
 
-        <p className="py-4">
-          Contact Us: If you have any questions about this Privacy Policy,
-          please contact us at www.worldweatherapp.com.
-        </p>
+          <p className="py-4">
+            Contact Us: If you have any questions about this Privacy Policy,
+            please contact us at www.worldweatherapp.com.
+          </p>
 
-        <p className="py-4">
-          <strong>Disclaimer</strong>
-        </p>
-        <p>
-          Accountability for content
-          <br />
-          The contents of our pages have been created with the utmost care.
-          However, we cannot guarantee the contents‘ accuracy, completeness or
-          topicality. According to statutory provisions, we are furthermore
-          responsible for our own content on these web pages. In this matter,
-          please note that we are not obliged to monitor the transmitted or
-          saved information of third parties, or investigate circumstances
-          pointing to illegal activity. Our obligations to remove or block the
-          use of information under generally applicable laws remain unaffected
-          by this as per §§ 8 to 10 of the Telemedia Act (TMG).
-        </p>
-        <p className="py-4">
-          <strong>Accountability for links</strong>
-        </p>
-        <p>
-          Responsibility for the content of external links (to web pages of
-          third parties) lies solely with the operators of the linked pages. No
-          violations were evident to us at the time of linking. Should any legal
-          infringement become known to us, we will remove the respective link
-          immediately.
-        </p>
+          <p className="py-4">
+            <strong>Disclaimer</strong>
+          </p>
+          <p>
+            Accountability for content
+            <br />
+            The contents of our pages have been created with the utmost care.
+            However, we cannot guarantee the contents‘ accuracy, completeness or
+            topicality. According to statutory provisions, we are furthermore
+            responsible for our own content on these web pages. In this matter,
+            please note that we are not obliged to monitor the transmitted or
+            saved information of third parties, or investigate circumstances
+            pointing to illegal activity. Our obligations to remove or block the
+            use of information under generally applicable laws remain unaffected
+            by this as per §§ 8 to 10 of the Telemedia Act (TMG).
+          </p>
+          <p className="py-4">
+            <strong>Accountability for links</strong>
+          </p>
+          <p>
+            Responsibility for the content of external links (to web pages of
+            third parties) lies solely with the operators of the linked pages.
+            No violations were evident to us at the time of linking. Should any
+            legal infringement become known to us, we will remove the respective
+            link immediately.
+          </p>
 
-        <p className="py-4">
-          <strong>Terms and Conditions</strong>
-        </p>
-        <p className="py-4">
-          By accessing and using this website, you agree to comply with and be
-          bound by the following terms and conditions:
-        </p>
-        <p className="py-4">
-          Intellectual Property: All content on this website, including text,
-          images, graphics, and logos, is the property of
-          www.worldweatherapp.com and is protected by copyright laws.
-        </p>
-        <p>
-          Use of Content: You may not reproduce, modify, distribute, or
-          republish any content from this website without prior written consent
-          from www.worldweatherapp.com.
-        </p>
-        <p className="py-4">
-          Disclaimer: The content on this website is provided "as is" without
-          warranties of any kind. www.worldweatherapp.com shall not be liable
-          for any errors or omissions in the content. Governing Law: These terms
-          and conditions are governed by the laws of Germany.
-        </p>
-        <p>
-          Contact Us: If you have any questions about these terms and
-          conditions, please contact us at www.worldweatherapp.com.
-        </p>
+          <p className="py-4">
+            <strong>Terms and Conditions</strong>
+          </p>
+          <p className="py-4">
+            By accessing and using this website, you agree to comply with and be
+            bound by the following terms and conditions:
+          </p>
+          <p className="py-4">
+            Intellectual Property: All content on this website, including text,
+            images, graphics, and logos, is the property of
+            www.worldweatherapp.com and is protected by copyright laws.
+          </p>
+          <p>
+            Use of Content: You may not reproduce, modify, distribute, or
+            republish any content from this website without prior written
+            consent from www.worldweatherapp.com.
+          </p>
+          <p className="py-4">
+            Disclaimer: The content on this website is provided "as is" without
+            warranties of any kind. www.worldweatherapp.com shall not be liable
+            for any errors or omissions in the content. Governing Law: These
+            terms and conditions are governed by the laws of Germany.
+          </p>
+          <p>
+            Contact Us: If you have any questions about these terms and
+            conditions, please contact us at www.worldweatherapp.com.
+          </p>
 
-        <p className="py-4">
-          <strong>Copyright</strong>
-        </p>
-        <p>
-          Our web pages and their contents are subject to German copyright law.
-          Unless expressly permitted by law, every form of utilizing,
-          reproducing or processing works subject to copyright protection on our
-          web pages requires the prior consent of the respective owner of the
-          rights. Individual reproductions of a work are only allowed for
-          private use. The materials from these pages are copyrighted and any
-          unauthorized use may violate copyright laws.
-        </p>
-        <p className="py-4">
-          <strong>© 2024 World Weather App</strong>
-        </p>
+          <p className="py-4">
+            <strong>Copyright</strong>
+          </p>
+          <p>
+            Our web pages and their contents are subject to German copyright
+            law. Unless expressly permitted by law, every form of utilizing,
+            reproducing or processing works subject to copyright protection on
+            our web pages requires the prior consent of the respective owner of
+            the rights. Individual reproductions of a work are only allowed for
+            private use. The materials from these pages are copyrighted and any
+            unauthorized use may violate copyright laws.
+          </p>
+          <p className="py-4">
+            <strong>© 2024 World Weather App</strong>
+          </p>
+        </Text>
       </div>
       <FooterComponent />
     </>
