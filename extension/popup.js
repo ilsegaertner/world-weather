0f92436b8130a2bca3c24f83e7859bfb9aa87573
Ilse Gaertner <joachimpruegl@gmail.com>
2024-08-04 13:26:34 +0200

    update remove write logic for appextension commit

--- extension/popup.js
+++ extension/popup.js
@@ -5,7 +5,6 @@ import AppExtension from "./App-extension";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <>
-    <h1>This should be rendered</h1>
     <AppExtension />
   </>
 );
